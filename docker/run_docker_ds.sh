e49e4e9a28537cd173c24738c7111ce26f202273
Liu Ke <liuke@tenstorrent.com>
2024-04-11 20:53:38 -0700

    megetron-lm gpt runs

---
+++ docker/run_docker_ds.sh
@@ -0,0 +1,30 @@
+# docker build -t megatron -f ./Dockerfile .
+# docker tag megatron:latest keliu354/megatron:v6
+# docker push keliu354/megatron:v6
+# docker system prune --volumes
+
+docker_img="megatron:latest"
+
+home_dir_0="${HOME}/Megatron-DeepSpeed-TT"
+docker_dir_0="/workspace/Megatron-DeepSpeed"
+
+home_dir_1="${HOME}/Megatron-LM"
+docker_dir_1="/workspace/Megatron-LM"
+
+home_dir_data="${HOME}/llm-datasets"
+docker_dir_data="/workspace/datasets"
+
+home_dir_model="${HOME}/llm-models"
+docker_dir_model="/workspace/models"
+
+work_dir=${docker_dir_0}
+
+docker run --gpus all --rm -it -P \
+    --cap-add SYS_NICE --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+    -v ${home_dir_0}:${docker_dir_0} \
+    -v ${home_dir_1}:${docker_dir_1} \
+    -v ${home_dir_data}:${docker_dir_data} \
+    -v ${home_dir_model}:${docker_dir_model} \
+    -w /${work_dir} \
+    ${docker_img} \
+    bash
--- docker/run_docker_lm.sh
+++ docker/run_docker_lm.sh
@@ -1,18 +1,21 @@
-# docker build -t megatron -f ./Dockerfile .
+# docker build -t megatron-lm -f ./Dockerfile_lm .
 # docker tag megatron:latest keliu354/megatron:v6
 # docker push keliu354/megatron:v6
 # docker system prune --volumes
 
-docker_img="megatron:latest"
+docker_img="megatron-lm:latest"
 
 home_dir_0="${HOME}/Megatron-DeepSpeed-TT"
 docker_dir_0="/workspace/Megatron-DeepSpeed"
 
-home_dir_1="${HOME}/llm-datasets"
-docker_dir_1="/workspace/datasets"
+home_dir_1="${HOME}/Megatron-LM"
+docker_dir_1="/workspace/Megatron-LM"
 
-home_dir_2="${HOME}/llm-models"
-docker_dir_2="/workspace/models"
+home_dir_data="${HOME}/llm-datasets"
+docker_dir_data="/workspace/datasets"
+
+home_dir_model="${HOME}/llm-models"
+docker_dir_model="/workspace/models"
 
 work_dir=${docker_dir_0}
 
@@ -20,7 +23,8 @@ docker run --gpus all --rm -it -P \
     --cap-add SYS_NICE --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     -v ${home_dir_0}:${docker_dir_0} \
     -v ${home_dir_1}:${docker_dir_1} \
-    -v ${home_dir_2}:${docker_dir_2} \
+    -v ${home_dir_data}:${docker_dir_data} \
+    -v ${home_dir_model}:${docker_dir_model} \
     -w /${work_dir} \
     ${docker_img} \
     bash
